572d8dd52a46d3656822d31b883b3379e595528d
madrury <matthew.drury.83@gmail.com>
2016-03-15 21:09:49 -0700

    Ch2 Exercises.

--- ch2-02.scala
+++ ch2-02.scala
@@ -8,8 +8,8 @@ def findFirst[A](as: Array[A], p: A => Boolean): Int = {
     loop(0)
 }
 
-println(findFirst(Array(0, 1, 2, 3, 4), (x: Int) => x == 3))
-println(findFirst(Array(0, 1, 2, 3, 4), (x: Int) => x % 2 == 1))
+// println(findFirst(Array(0, 1, 2, 3, 4), (x: Int) => x == 3))
+// println(findFirst(Array(0, 1, 2, 3, 4), (x: Int) => x % 2 == 1))
 
 
 /* Check if an array A is sorted with respoect to a binary predicate */
@@ -22,5 +22,44 @@ def isSorted[A](as: Array[A], p: (A, A) => Boolean): Boolean = {
     loop(1)
 }
 
-println(isSorted(Array(0, 1, 2, 3, 4), (x: Int , y: Int) => x < y))
-println(isSorted(Array(0, 1, 3, 2, 4), (x: Int , y: Int) => x < y))
+// println(isSorted(Array(0, 1, 2, 3, 4), (x: Int , y: Int) => x < y))
+// println(isSorted(Array(0, 1, 3, 2, 4), (x: Int , y: Int) => x < y))
+
+
+/* Curry a function */
+def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    def g(a: A): B => C = {
+        def h(b: B): C = {
+            f(a, b)
+        }
+        h
+    }
+    g
+}
+
+// println(curry((x: Int , y: Int) => x + y)(1)(2))
+// println(curry((x: Int , y: Int) => x + y)(2)(2))
+
+
+/* Uncurry a function */
+def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    def g(a: A, b: B): C = {
+        f(a)(b)
+    }
+    g
+}
+
+// println(uncurry((x: Int) => ((y: Int) => x + y))(1, 1)) 
+// println(uncurry((x: Int) => ((y: Int) => x + y))(1, 2)) 
+
+
+/* Function composition */
+def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    def h(a: A): C = {
+        f(g(a))
+    }
+    h
+}
+
+println(compose((x: Int) => 2 * x, (x: Int) => 2 * x)(1))
+println(compose((x: Int) => 2 * x, (x: Int) => 2 * x)(2))
