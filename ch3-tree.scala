9405711ca9302b695d45f8fdaa3248b4e1b91cd6
madrury <matthew.drury.83@gmail.com>
2016-03-20 19:12:01 -0700

    Finish tree exercises.

--- ch3-tree.scala
+++ ch3-tree.scala
@@ -29,6 +29,25 @@ object Tree {
         case Branch(left, right) => Branch(Tree.map(left, f), Tree.map(right, f))
     }
 
+    // Ex 2.29
+    def fold[A, B, C](tree: Tree[A])(f: A => B, g: (B, B) => B): B = tree match {
+        case Leaf(a) => f(a)
+        case Branch(left, right) => g(Tree.fold(left)(f, g), Tree.fold(right)(f, g))
+    }
+    def sizeWithFold[A](tree: Tree[A]): Int = {
+        Tree.fold(tree)((a: A) => 1, (z1: Int, z2: Int) => z1 + z2)
+    }
+    def maximumWithFold(tree: Tree[Int]): Int = {
+        Tree.fold(tree)((a: Int) => a, (z1: Int, z2:Int) => z1 max z2)
+    }
+    def depthWithFold[A](tree: Tree[A]): Int = {
+        Tree.fold(tree)((a: A) => 0, (z1: Int, z2: Int) => z1 max z2 + 1)
+    }
+    def mapWithFold[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+        Tree.fold(tree)((a: A) => Leaf(f(a)), 
+                        (left: Tree[B], right: Tree[B]) => Branch(left, right))
+    }
+
 }
 
 import Tree._
@@ -41,16 +60,25 @@ val t2:Tree[Int] = Branch(
 ) 
 
 // Ex 2.25
-println(Tree.size(t))
-println(Tree.size(t2))
+//println(Tree.size(t))
+//println(Tree.size(t2))
 
 // Ex 2.26
-println(Tree.maximum(t))
+//println(Tree.maximum(t))
 
 // Ex 2.27
-println(Tree.depth(t))
-println(Tree.depth(t2))
+//println(Tree.depth(t))
+//println(Tree.depth(t2))
 
 // Ex 2.28
-println(Tree.map(t, (x: Int) => 2 * x))
-println(Tree.map(t2, (x: Int) => 2 * x))
+//println(Tree.map(t, (x: Int) => 2 * x))
+//println(Tree.map(t2, (x: Int) => 2 * x))
+
+// Ex 2.29
+println(Tree.sizeWithFold(t))
+println(Tree.sizeWithFold(t2))
+println(Tree.maximumWithFold(t))
+println(Tree.depthWithFold(t))
+println(Tree.depthWithFold(t2))
+println(Tree.mapWithFold(t)((x: Int) => 2 * x))
+println(Tree.mapWithFold(t2)((x: Int) => 2 * x))
