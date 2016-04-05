a2811c0bde375b267db411a47ca3820cf8ef9a4f
madrury <matthew.drury.83@gmail.com>
2016-04-04 20:45:20 -0700

    Ex 4.6

--- ch4-either.scala
+++ ch4-either.scala
@@ -1,15 +1,30 @@
 sealed trait Either[+E, +A] {
 
-    // Ex 4.7
+    // Ex 4.6
     def map[B](f: A => B): Either[E, B] = this match {
         case Left(e) => Left(e)
         case Right(a) => Right(f(a))
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+        case Left(e) => Left(e)
+        case Right(a) => f(a)
+    }
+
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+        case Left(e) => b
+        case Right(a) => Right(a)
     }
 
 }
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
+object Either {
+    def map2[E, A, B, C](a: Either[E, A], b: Either[E, B])(f: (A, B) => C): Either[E, C] = {
+        a flatMap (aa => (b map (bb => f(aa, bb)))) 
+    }
+}
 
 import Either._
 
@@ -17,5 +32,21 @@ val x:Either[String, Int] = Right(1)
 val e:Either[String, Int] = Left("Error!")
 
 // Ex 4.6
-println(x.map((x:Int) => 2 * x))
-println(e.map((x:Int) => 2 * x))
+// println(x.map((x:Int) => 2 * x))
+// println(e.map((x:Int) => 2 * x))
+
+//println(x.flatMap((x:Int) => Right(2 * x)))
+//println(x.flatMap((x:Int) => Left("Error!")))
+//println(e.flatMap((x:Int) => Right(2*x)))
+//println(e.flatMap((x:Int) => Left("Error!")))
+
+//println(x.orElse(Right(1)))
+//println(x.orElse(Left("Error!")))
+//println(e.orElse(Right(1)))
+//println(e.orElse(Left("Error!")))
+
+val f = ((x:Int, y:Int) => x + y)
+println(map2(x, x)(f))
+println(map2(e, x)(f))
+println(map2(x, e)(f))
+println(map2(e, e)(f))
