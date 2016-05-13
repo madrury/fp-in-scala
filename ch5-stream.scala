df673ea685e72ef36cea5894d60aca0894a9dd80
madrury <matthew.drury.83@gmail.com>
2016-05-12 22:03:50 -0700

    Add Stream.take

--- ch5-stream.scala
+++ ch5-stream.scala
@@ -8,6 +8,20 @@ sealed trait Stream[+A] {
         case Cons(h, t) => h() :: t().toList
     }
 
+    // 5.2: take the first n elements of a stream
+    // Assumes that n >= 0
+    def unsafeTake(n: Int): Stream[A] = {
+        if (n == 0) return Stream.empty
+        else return this match {
+            case Empty => Stream.empty
+            case Cons(h, t) => Stream.cons(h(), t().unsafeTake(n - 1)) 
+        }
+    }
+    def take(n: Int): Option[Stream[A]] = {
+        if (n < 0) {return None}
+        else Some(this.unsafeTake(n))
+    }
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
